99f825c78a5b1550c2422fc43b6bf39f1d57ec00
letItCurl <rolandlopez.developer@gmail.com>
2020-05-23 16:07:50 +0200

    working on sort

---
+++ lib/tri/tri.rb
@@ -0,0 +1,17 @@
+class Tri
+    def initialize(arg)
+        @arr = arg.dup
+    end
+    
+    def perform
+        lmax = @arr.length - 1
+        for i in (0..lmax) do
+            for j in (0..lmax) do
+                
+            end
+        end
+    end
+end
+
+f = Tri.new(ARGV)
+f.perform
