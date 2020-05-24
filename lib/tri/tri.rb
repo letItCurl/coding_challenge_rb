c9c998deefbab48357e2b788bc8db394e9007d02
letItCurl <rolandlopez.developer@gmail.com>
2020-05-24 12:15:21 +0200

    swap..

--- lib/tri/tri.rb
+++ lib/tri/tri.rb
@@ -4,11 +4,22 @@ class Tri
         @sorted = []
     end
     
+    def swap a, b
+        @arr[a], @arr[b] = @arr[b], @arr[a]
+    end
+
     def perform
-        #.bsearsh
-        #.index
-        #.insert
-        #.permutation
+        @len = @arr.size
+        @range = (0..@len-1)
+        for index in @range
+            @arr_range = @arr.slice(1 , @len - index)
+            @max = @arr_range.max
+            @index = @arr_range.each_with_index.max[1]
+            puts "#run #{index}"
+            puts "value: #{@max} / #{@index}"
+            swap @arr_range.size, @index
+        end
+        puts @arr
     end
 end
 
