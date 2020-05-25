ccd5f038d30ef89819997d12eb3008d8c29ac39e
letItCurl <rolandlopez.developer@gmail.com>
2020-05-25 16:26:44 +0200

    consecutive_string

--- lib/ideas/persistent_bugger.rb
+++ lib/ideas/persistent_bugger.rb
@@ -24,6 +24,6 @@ def persistence(n)
 end
 
 def persistence_v2(n)
-    n < 10 ? 0 : 1 + persistence(n.to_s.chars.map(&:to_i).reduce(:*))
-  end
+  n < 10 ? 0 : 1 + persistence(n.to_s.chars.map(&:to_i).reduce(:*))
+end
   
